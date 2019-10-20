697a5c47afcb6ec7d764e2834e908e01360618f1
Conal RYAN <conal.ryan@amadeus.com>
2019-10-19 22:18:21 -0400

    test all template combinations

--- projects/examples/src/app/calendargrid/ex-calendargrid.component.ts
+++ projects/examples/src/app/calendargrid/ex-calendargrid.component.ts
@@ -8,12 +8,10 @@ import { calendarGridData, calendarGridNestedData, calendarGridDoubleNestedData 
     <h2>Calendar Grid Wrapper</h2>
 
     <h4>No templates (use with primitive value)</h4>
-
     <cr-calendar-grid [calendarGridData]="calendarGridData"></cr-calendar-grid>
-
     <br/><br/>
 
-    <!--<h4>Single template for all rows</h4>
+    <h4>Single template for all rows</h4>
     <cr-calendar-grid [calendarGridData]="calendarGridData">
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
@@ -31,38 +29,38 @@ import { calendarGridData, calendarGridNestedData, calendarGridDoubleNestedData 
     <cr-calendar-grid [calendarGridData]="calendarGridData">
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr1 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
+          cr1 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
 
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr2 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
+          cr2 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
 
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr3 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          <span *ngIf="c?.value">TRUE</span>
+          cr3 <span *ngIf="c?.value">TRUE</span>
         </ng-template>
       </cr-calendar-grid-row>
-    </cr-calendar-grid>-->
+    </cr-calendar-grid>
 
     <br/><br/>
 
-    <!--<h3>Nested Data: Row 1 single, Row 2 one nest, Row 3 two level nest</h3>
+    <h3>Single Nested Data</h3>
+
     <h4>No templates (use with primitive value)</h4>
     <cr-calendar-grid [calendarGridData]="calendarGridNestedData"></cr-calendar-grid>
-
     <br/><br/>
 
     <h4>Single template for all rows</h4>
@@ -83,38 +81,38 @@ import { calendarGridData, calendarGridNestedData, calendarGridDoubleNestedData 
     <cr-calendar-grid [calendarGridData]="calendarGridNestedData">
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr1 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
+          cr1 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
 
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr2 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
+          cr2 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
 
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr3 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          <span *ngIf="c?.value">TRUE</span>
+          cr3 <span *ngIf="c?.value">TRUE</span>
         </ng-template>
       </cr-calendar-grid-row>
-    </cr-calendar-grid>-->
+    </cr-calendar-grid>
 
-    <!--<br/><br/>
+    <br/><br/>
 
     <h3>Double Nested Data</h3>
+
     <h4>No templates (use with primitive value)</h4>
     <cr-calendar-grid [calendarGridData]="calendarGridDoubleNestedData"></cr-calendar-grid>
-
     <br/><br/>
 
     <h4>Single template for all rows</h4>
@@ -135,122 +133,116 @@ import { calendarGridData, calendarGridNestedData, calendarGridDoubleNestedData 
     <cr-calendar-grid [calendarGridData]="calendarGridDoubleNestedData">
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr1 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
+          cr1 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
 
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr2 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
+          cr2 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
 
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr3 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          <span *ngIf="c?.value">TRUE</span>
+          cr3 <span *ngIf="c?.value">TRUE</span>
         </ng-template>
       </cr-calendar-grid-row>
     </cr-calendar-grid>
 
     <br/><br/>
 
-    <h3>Double Nested Data: Experimenting with multi label/cells per row</h3>
+    <h3>Double Nested Data: Multi label/cells per row</h3>
 
-    <h4 style="background-color:blue">Two labels per row</h4>
-    <h4 [ngStyle]="{'color': 'blue'}">Two labels per row</h4>
-    <h4 class="blue-it">Two labels per row</h4>
-    <br/><br/>
-
-    <cr-calendar-grid [calendarGridData]="calendarGridDoubleNestedData">
-
-      <cr-calendar-grid-row>
-        <ng-template crCalendarGridLabel let-l="label">
-          <span>First: {{ l }}</span>
-        </ng-template>
-
-        <ng-template crCalendarGridLabel let-l="label">
-          <span>Second: {{ l }}</span>
-        </ng-template>
-
-        <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
-        </ng-template>
-      </cr-calendar-grid-row>
-    </cr-calendar-grid>-->
-
-    <br/><br/>
-
-    <!--<h4>Template for each row</h4>
-    <br/><br/>
+    <h4>Two labels per row</h4>
     <cr-calendar-grid [calendarGridData]="calendarGridDoubleNestedData">
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr1 a <span>First: {{ l }}</span>
         </ng-template>
-        <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
-        </ng-template>
-      </cr-calendar-grid-row>
 
-      <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr1 b <span>Second: {{ l }}</span>
         </ng-template>
-        <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
-        </ng-template>
-      </cr-calendar-grid-row>
 
-      <cr-calendar-grid-row>
-        <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
-        </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          <span *ngIf="c?.value">TRUE</span>
+          cr1 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
     </cr-calendar-grid>
 
+    <br/><br/>
+
+    <h4>Template for each row</h4>
+    <cr-calendar-grid [calendarGridData]="calendarGridDoubleNestedData">
+      <cr-calendar-grid-row>
+        <ng-template crCalendarGridLabel let-l="label">
+          lr1 {{ l }}
+        </ng-template>
+        <ng-template crCalendarGridCell let-c="cell">
+          cr1 {{ c?.value }}
+        </ng-template>
+      </cr-calendar-grid-row>
+
+      <cr-calendar-grid-row>
+        <ng-template crCalendarGridLabel let-l="label">
+          lr2 {{ l }}
+        </ng-template>
+        <ng-template crCalendarGridCell let-c="cell">
+          cr2 {{ c?.value }}
+        </ng-template>
+      </cr-calendar-grid-row>
+
+      <cr-calendar-grid-row>
+        <ng-template crCalendarGridLabel let-l="label">
+          lr3 {{ l }}
+        </ng-template>
+        <ng-template crCalendarGridCell let-c="cell">
+          cr3 <span *ngIf="c?.value">TRUE</span>
+        </ng-template>
+      </cr-calendar-grid-row>
+    </cr-calendar-grid>
+
+    <br/><br/>
 
     <h4>Template for each row Calendar Grid Class</h4>
-    <br/><br/>
     <cr-calendar-grid-class [calendarGridData]="calendarGridDoulbeNestedDataClass">
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr1 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
+          cr1 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
 
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr2 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          {{ c?.value }}
+          cr2 {{ c?.value }}
         </ng-template>
       </cr-calendar-grid-row>
 
       <cr-calendar-grid-row>
         <ng-template crCalendarGridLabel let-l="label">
-          {{ l }}
+          lr3 {{ l }}
         </ng-template>
         <ng-template crCalendarGridCell let-c="cell">
-          <span *ngIf="c?.value">TRUE</span>
+          cr3 <span *ngIf="c?.value">TRUE</span>
         </ng-template>
       </cr-calendar-grid-row>
-    </cr-calendar-grid-class>-->
+    </cr-calendar-grid-class>
   `,
   styles: [`
     #blue-it {
@@ -287,8 +279,5 @@ export class ExCalendargridComponent {
 
   calendarGridDoulbeNestedDataClass: CalendarGridDataClass = new CalendarGridDataClass(calendarGridDoubleNestedData().rows);
 
-  constructor() {
-    // console.log(this.calendarGridDoubleNestedData);
-    // console.log(this.calendarGridDoulbeNestedDataClass);
-   }
+  constructor() {}
 }
