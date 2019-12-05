3d63fd044bdc2f466d780ae87cdb3a4a8d7052d4
Conal RYAN <conaleryan@gmail.com>
2019-12-05 16:23:14 -0500

    Update examples

--- projects/examples/src/app/calendar-ex/calendar-ex.component.ts
+++ projects/examples/src/app/calendar-ex/calendar-ex.component.ts
@@ -1,79 +1,191 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
-import { CalendarGridCell } from 'projects/crangular/src/lib/calendar-grid/calendar-grid';
-import { DateUtil } from 'projects/crangular/src/lib/util/date.util';
 
 @Component({
-  selector: 'cr-calendar-ex',
+  selector: 'app-calendar-ex',
   template: `
-    <ng-template #customDay let-date let-currentMonth="currentMonth" let-selected="selected" let-disabled="disabled" let-focused="focused">
-      <span class="custom-day-co">
-        <span class="d-block">{{ date.day }}</span>
-        <span class="d-block custom-cell-co">{{ cell(date) | number: '1.2'}}</span>
-      </span>
-    </ng-template>
+    <div class="m-3">
+      <p>To scale ngb-datepicker add the [scale] attribute and optionally [monthsPerRow] atrribute if you want to break months per rows</p>
+      <p>To style ngb-datepicker wrap in cr-calendar element</p>
 
-    <h3>One Month</h3>
-    <cr-calendar>
+      <ng-template #customDay let-date let-currentMonth="currentMonth" let-selected="selected" let-disabled="disabled" let-focused="focused">
+        <span class="custom-day-co">
+          <span class="d-block">{{ date.day }}</span>
+          <span class="d-block custom-cell-co">{{ cell(date) | number: '1.2'}}</span>
+        </span>
+      </ng-template>
+
+      <div class="row">
+        <div class="col-4">
+          <h3>One Month 2X Unstyled</h3>
+          <ngb-datepicker
+            [dayTemplate]="customDay"
+            [displayMonths]="1"
+            [firstDayOfWeek]="7"
+            [maxDate]="maxNov"
+            [minDate]="minNov"
+            [navigation]="'select'"
+            [outsideDays]="'hidden'"
+            [scale]="2" 
+            (navigate)="onNavigate($event)"
+            (select)="onSelect($event)">
+          </ngb-datepicker>
+        </div>
+
+        <div class="col-4">
+          <h3>One Month 2.5X Styled</h3>
+          <cr-calendar>
+            <ngb-datepicker
+              [dayTemplate]="customDay"
+              [displayMonths]="1"
+              [firstDayOfWeek]="7"
+              [maxDate]="maxApr"
+              [minDate]="minNov"
+
+              [navigation]="'arrows'"
+              [scale]="2.5" 
+              [startDate]="minNov"
+              (navigate)="onNavigate($event)"
+              (select)="onSelect($event)">
+            </ngb-datepicker>
+          </cr-calendar>
+        </div>
+
+        <div class="col-4">
+          <h3>One Month 3X Styled Static</h3>
+          <cr-calendar>
+            <ngb-datepicker
+              [dayTemplate]="customDay"
+              [displayMonths]="1"
+              [firstDayOfWeek]="7"
+              [maxDate]="maxNov"
+              [minDate]="minNov"
+              [monthsPerRow]="1"
+              [navigation]="'none'"
+              [outsideDays]="'hidden'"
+              [scale]="3" 
+              (navigate)="onNavigate($event)"
+              (select)="onSelect($event)">
+            </ngb-datepicker>
+          </cr-calendar>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="col-4">
+          <pre>
+            <code>
+              &lt;ngb-datepicker
+                [dayTemplate]="customDay"
+                [displayMonths]="1"
+                [firstDayOfWeek]="7"
+                [maxDate]="maxNov"
+                [minDate]="minNov"
+                [monthsPerRow]="1"
+                [navigation]="'select'"
+                [outsideDays]="'hidden'"
+                [scale]="2" 
+                (navigate)="onNavigate($event)"
+                (select)="onSelect($event)"&gt;
+              &lt;/ngb-datepicker>
+            </code>
+          </pre>
+        </div>
+
+        <div class="col-4">
+          <pre>
+            <code>
+              &lt;cr-calendar&gt;
+                &lt;ngb-datepicker
+                  [dayTemplate]="customDay"
+                  [displayMonths]="1"
+                  [firstDayOfWeek]="7"
+                  [maxDate]="maxApr"
+                  [minDate]="minNov"
+                  [monthsPerRow]="1"
+                  [navigation]="'arrows'"
+                  [scale]="2.5" 
+                  [startDate]="minNov"
+                  (navigate)="onNavigate($event)"
+                  (select)="onSelect($event)"&gt;
+                &lt;/ngb-datepicker&gt;
+              &lt;/cr-calendar&gt;
+            </code>
+          </pre>
+        </div>
+
+        <div class="col-4">
+          <pre>
+            <code>
+              &lt;cr-calendar&gt;
+                &lt;ngb-datepicker
+                  [dayTemplate]="customDay"
+                  [displayMonths]="1"
+                  [firstDayOfWeek]="7"
+                  [maxDate]="maxNov"
+                  [minDate]="minNov"
+                  [monthsPerRow]="1"
+                  [navigation]="'none'"
+                  [outsideDays]="'hidden'"
+                  [scale]="3" 
+                  (navigate)="onNavigate($event)"
+                  (select)="onSelect($event)"&gt;
+                &lt;/ngb-datepicker&gt;
+              &lt;/cr-calendar&gt;
+            </code>
+          </pre>
+        </div>
+      </div>
+
+      <h3>Six Months 3X Styled Static</h3>
+      <cr-calendar>
+        <ngb-datepicker
+          [dayTemplate]="customDay"
+          [displayMonths]="6"
+          [firstDayOfWeek]="7"
+          [maxDate]="maxFeb"
+          [minDate]="minNov"
+          [monthsPerRow]="3"
+          [navigation]="'none'"
+          [outsideDays]="'hidden'"
+          [scale]="3" 
+          (navigate)="onNavigate($event)"
+          (select)="onSelect($event)">
+        </ngb-datepicker>
+      </cr-calendar>
+
+      <pre>
+        <code>
+          &lt;cr-calendar&gt;
+            &lt;ngb-datepicker
+              [dayTemplate]="customDay"
+              [displayMonths]="6"
+              [firstDayOfWeek]="7"
+              [maxDate]="maxFeb"
+              [minDate]="minNov"
+              [monthsPerRow]="3"
+              [navigation]="'none'"
+              [outsideDays]="'hidden'"
+              [scale]="3" 
+              (navigate)="onNavigate($event)"
+              (select)="onSelect($event)"&gt;
+            &lt;/ngb-datepicker&gt;
+          &lt;/cr-calendar&gt;
+        </code>
+      </pre>
+      
+      <h4>Styles do not leak to other ngb-datepickers</h4>
       <ngb-datepicker
-        [dayTemplate]="customDay"
         [displayMonths]="1"
         [firstDayOfWeek]="7"
         [maxDate]="maxApr"
         [minDate]="minNov"
-        [monthsPerRow]="1"
-        [navigation]="'arrows'"
-        [scale]="2" 
         [startDate]="minNov"
         (navigate)="onNavigate($event)"
         (select)="onSelect($event)">
       </ngb-datepicker>
-    </cr-calendar>
-
-    <h3>One Month Static</h3>
-    <cr-calendar>
-      <ngb-datepicker
-        [dayTemplate]="customDay"
-        [displayMonths]="1"
-        [firstDayOfWeek]="7"
-        [maxDate]="maxNov"
-        [minDate]="minNov"
-        [monthsPerRow]="1"
-        [navigation]="'none'"
-        [outsideDays]="'hidden'"
-        [scale]="2" 
-        (navigate)="onNavigate($event)"
-        (select)="onSelect($event)">
-      </ngb-datepicker>
-    </cr-calendar>
-
-    <h3>Six Months Static</h3>
-    <cr-calendar>
-      <ngb-datepicker
-        [dayTemplate]="customDay"
-        [displayMonths]="6"
-        [firstDayOfWeek]="7"
-        [maxDate]="maxFeb"
-        [minDate]="minNov"
-        [monthsPerRow]="3"
-        [navigation]="'none'"
-        [outsideDays]="'hidden'"
-        [scale]="2" 
-        (navigate)="onNavigate($event)"
-        (select)="onSelect($event)">
-      </ngb-datepicker>
-    </cr-calendar>
-    
-    <h4>Styles do not leak to other ngb-datepickers</h4>
-    <ngb-datepicker
-      [displayMonths]="1"
-      [firstDayOfWeek]="7"
-      [maxDate]="maxApr"
-      [minDate]="minNov"
-      [startDate]="minNov"
-      (navigate)="onNavigate($event)"
-      (select)="onSelect($event)">
-    </ngb-datepicker>
+    </div>
   `,
   styles: [`
     h3, h4 {
@@ -86,12 +198,15 @@ import { DateUtil } from 'projects/crangular/src/lib/util/date.util';
     .custom-cell-co {
       color: blue;
     }
+    pre {
+      background-color: #d2d2d2;
+    }
   `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CalendarExComponent {
 
-  cells: CalendarGridCell<number>[] = [];
+  cells = [];
   minNov = new NgbDate(2019, 11, 1);
   maxNov = new NgbDate(2019, 11, 30);
 
@@ -127,11 +242,11 @@ export class CalendarExComponent {
     console.log($event);
   }
 
-  private _generateData(year: number, month: number, days: number): CalendarGridCell<number>[] {
-    const cells: CalendarGridCell<number>[] = [];
+  private _generateData(year: number, month: number, days: number) {
+    const cells = [];
     for (let i = 1; i <= days; i++) {
       const d = new Date(year, month, i, 12);
-      const cell: CalendarGridCell<number> = {
+      const cell = {
         label: d.toISOString(),
         date: d,
         value: Math.random() * 100
@@ -143,7 +258,7 @@ export class CalendarExComponent {
 
   cell(date: NgbDate): any {
     const cell = this.cells.find(cell => {
-      return DateUtil.areDatesEqual(DateUtil.dateToNgbDateStruct(cell.date), date);
+      return cell.date.getFullYear() === date.year && cell.date.getMonth() + 1 === date.month && cell.date.getDate() === date.day;
     });
     return cell ? cell.value : 0;
   }
--- projects/examples/src/app/calendar-grid-ex/calendar-grid-model-ex.component.ts
+++ projects/examples/src/app/calendar-grid-ex/calendar-grid-model-ex.component.ts
@@ -3,12 +3,95 @@ import { CalendarGridData } from 'projects/crangular/src/lib/calendar-grid/calen
 import { calendarGridDoubleNestRows, calendarGridRows, calendarGridSingleNestRows } from './calendar-grid-data.stub';
 
 @Component({
-  selector: 'cr-calendar-grid-model-ex',
+  selector: 'app-calendar-grid-model-ex',
   template: `
-    <h3>Model</h3>
+    <h3>Interfaces</h3>
+    <pre>
+      <code>
+        export interface CalendarGridCell&lt;T&gt; &#123;
+          label: string;
+          date: Date;
+          value: T;
+        &#125;
+        
+        export interface CalendarGridRow&lt;T&gt; &#123;
+          label: string;
+          cells: CalendarGridCell&lt;T&gt;[];
+          nodes?: CalendarGridRow&lt;any&gt;[];
+        &#125;
+      </code>
+    </pre>
+
+    <h3>Calendar grid data class (aka the brain)</h3>
+    <p>You must pass an instance of CalendarGridData class into cr-calendar-grid component</p>
+    <p>You must pass an array of CalendarGirdRow to the CalendarGridData constructor</p>
+    <p>To create rows, either create data using the interfaces or use the static methods CalendarGridData.cells and CalendarGridData.row respectively (examples below)</p>
+    <pre>
+      <code>
+        export class CalendarGridData &#123;
+          static cells = &lt;T&gt;(label: string, startDate: Date, numDays?: number, data?: T[]): CalendarGridCell&lt;T&gt;[];
+        
+          static row = &lt;T&gt;(label: string, startDate: Date, numDays?: number, data?: T[]): CalendarGridRow&lt;T&gt;;
+        
+          constructor(calendarGridRows: CalendarGridRow&lt;any&gt;[]);
+        
+          get rowsRaw(): CalendarGridRow&lt;any&gt;[];
+        
+          get rows(): CalendarGridRow&lt;any&gt;[];
+        
+          get visibleRows(): BitMask;
+        
+          get parentRows(): BitMask;
+        
+          index(calendarGridRow: CalendarGridRow&lt;any&gt;): number;
+        
+          isRowVisible(index: number): boolean;
+        
+          toggleRowVisibility(index: number): void;
+        
+          toggleNodesVisibility(index: number): void;
+        
+          levels(index: number): number[];
+        &#125;
+      </code>
+    </pre>
+
+    <h5>Generate emtpy cells from start date and number of days</h5>
+    <pre>
+      <code>
+        const cellsFromRange = CalendarGridData.cells('Codes', new Date(2019, 11, 1, 12), 7);
+      </code>
+    </pre>
+
+    <h5>Generate cells from array of data</h5>
+    <pre>
+      <code>
+        const cellsFromData = CalendarGridData.cells('Codes', new Date(2019, 11, 1, 12), null, ['AB', 'CD', 'EF']);
+      </code>
+    </pre>
+
+    <h5>Generate row with emtpy cells from start date and number of days</h5>
+    <pre>
+      <code>
+        const rowFromRange = CalendarGridData.row('Codes', new Date(2019, 11, 1, 12), 7);
+      </code>
+    </pre>
+
+    <h5>Generate cells from array of data</h5>
+    <pre>
+      <code>
+        const rowFromData = CalendarGridData.row('Codes', new Date(2019, 11, 1, 12), null, ['AB', 'CD', 'EF']);
+      </code>
+    </pre>
+
   `,
   styles: [`
-    
+    h3, h5 {
+      margin-top: 2rem;
+    }
+    pre {
+      background-color: #d2d2d2;
+    }
   `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
